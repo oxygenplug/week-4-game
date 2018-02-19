5392f4271dd33fcb5c50db26acc4f93007a4dab9
oxygenplug <dan.garces2@gmail.com>
2018-02-19 13:29:34 -0500

    set up click events to select hero and enemy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-
+// initialized two vars, hero and enemy. player's flirst click will set a character obj as the hero, second click with set character obj as enemy
     var hero;
     var enemy;
 
-
+// set up character objs
 
     var rey = {
         hp: 120,
@@ -97,5 +97,41 @@ $(document).ready(function () {
         },
     };
 
+    // click event for hero selection
+    $("#rey-btn").click(function() {
+        if (hero = undefined) {
+            hero = rey;
+        }
+        else {
+            enemy = rey;
+        };
+    })
+
+    $("#kylo-btn").click(function() {
+        if (hero = undefined) {
+            hero = kylo;
+        }
+        else {
+            enemy = kylo;
+        };
+    })
+
+    $("#maul-btn").click(function() {
+        if (hero = undefined) {
+            hero = maul;
+        }
+        else {
+            enemy = maul;
+        };
+    })
+
+    $("#vader-btn").click(function() {
+        if (hero = undefined) {
+            hero = vader;
+        }
+        else {
+            enemy = vader;
+        };
+    })
 
 })    
