6a7158b1206f1457d31e1e7be9fadc90abc5a909
oxygenplug <dan.garces2@gmail.com>
2018-02-19 13:23:53 -0500

    set up hero objs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,101 @@
+$(document).ready(function () {
+
+    var hero;
+    var enemy;
 
 
-$(document).ready(function() {
 
+    var rey = {
+        hp: 120,
+        atk: 10,
+        counterAtk: 7,
+        isEnemy = true,
+        isDefeated = false,
+        attack: function () {
+            if (isEnemy) {
+                hero.hp  = hero.hp - this.counterAtk;
+            }
+
+            else {
+                enemy.hp = enemy.hp - this.atk; 
+            };
+
+        },
+
+        checkifDefeated: function() {
+            if (hp == 0) {
+                this.isDefeated = true;
+            }     
+
+        },
+
+    };
+
+    var vader = {
+        hp: 80,
+        atk: 18,
+        counterAtk: 12,
+        attack: function () {
+            if (isEnemy) {
+                hero.hp  = hero.hp - this.counterAtk;
+            }
+
+            else {
+                enemy.hp = enemy.hp - this.atk; 
+            };
+
+        },
+        checkifDefeated: function() {
+            if (hp == 0) {
+                this.isDefeated = true;
+            }     
+
+        },
+    };
+
+    var kylo = {
+        hp: 100,
+        atk: 11,
+        counterAtk: 9,
+        attack: function () {
+            if (isEnemy) {
+                hero.hp  = hero.hp - this.counterAtk;
+            }
+
+            else {
+                enemy.hp = enemy.hp - this.atk; 
+            };
+
+        },
+        checkifDefeated: function() {
+            if (hp == 0) {
+                this.isDefeated = true;
+            }     
+
+        },
+    };
+
+    var maul = {
+        hp: 60,
+        atk: 20,
+        counterAtk: 15,
+        attack: function () {
+            if (isEnemy) {
+                hero.hp  = hero.hp - this.counterAtk;
+            }
+
+            else {
+                enemy.hp = enemy.hp - this.atk; 
+            };
+
+        },
+        checkifDefeated: function() {
+            if (hp == 0) {
+                this.isDefeated = true;
+            }     
+
+        },
+    };
 
 
 })    
