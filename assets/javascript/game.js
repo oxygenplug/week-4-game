52e3a2e525bc6838217c92ec3f51d6f7e355be0e
oxygenplug <dan.garces2@gmail.com>
2018-02-19 22:40:28 -0500

    tried using constructors vs a million separate objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,147 +1,107 @@
 $(document).ready(function () {
-// initialized two vars, hero and enemy. player's flirst click will set a character obj as the hero, second click with set character obj as enemy
     var hero;
     var enemy;
 
-// set up character objs
-
-     var rey = {
-        hp: 120,
-        atk: 10,
-        counterAtk: 7,
-        isEnemy: true,
-        isDefeated: false,
-
-        /* attack function. first checks to see if the character selected is an enemy. if it's an enemy, their attack type used will be the counter attack.
-        if they are not the enemy, it means they are the hero and that they can use the regular attack that grows in power each time it is used */
-
-        attack: function (enemy) {
-            
-            enemy.hp -= this.atk;
-
-        },
-
-        checkifDefeated: function() {
-            if (hp == 0) {
-                this.isDefeated = true;
-            }     
-
-        },
-
-    };
-
-    var vader = {
-        hp: 80,
-        atk: 18,
-        counterAtk: 12,
-        isEnemy: true,
-        isDefeated: false,
-        attack: function () {
+    function Character(hp, atk, counterAtk, name, imgURL) {
+        this.hp = hp;
+        this.atk = atk;
+        this.counterAtk = counterAtk;
+        this.isDefeated = false;
+        this.name = "";
+        this.imgURL = "";
+        this.attack = function () {
             if (isEnemy) {
-                hero.hp  = hero.hp - this.counterAtk;
+                hero.hp = hero.hp - this.counterAtk;
             }
 
             else {
-                enemy.hp = enemy.hp - this.atk; 
+                enemy.hp = enemy.hp - this.atk;
             };
 
-        },
-        checkifDefeated: function() {
+        };
+        this.checkifDefeated = function () {
             if (hp == 0) {
                 this.isDefeated = true;
-            }     
-
-        },
-    };
-
-    var kylo = {
-        hp: 100,
-        atk: 11,
-        counterAtk: 9,
-        isEnemy: true,
-        isDefeated: false,
-        attack: function () {
-            if (isEnemy) {
-                hero.hp  = hero.hp - this.counterAtk;
             }
+        };
+    }
 
-            else {
-                enemy.hp = enemy.hp - this.atk; 
-            };
+    var rey = new Character(120, 10, 7, "Rey", "../images/rey.jpg");
+    var kylo = new Character(100, 11, 9, "Kylo Ren", "../images/kyloRen.jpg");
+    var maul = new Character(60, 20, 15, "Darth Maul", "../images/darthMaul.png");
+    var vader = new Character(80, 18, 10, "Darth Vader", "../images/vader.jpg");
+
+
+    function Battle(hero, enemy) {
+        this.left = hero;
+        this.right = enemy;
+        this.start = function (left, right) {
+            this.left = left;
+            this.right = right;
+            this.displayCombatants();
 
         },
-        checkifDefeated: function() {
-            if (hp == 0) {
-                this.isDefeated = true;
-            }     
-
-        },
-    };
-
-    var maul = {
-        hp: 60,
-        atk: 20,
-        counterAtk: 15,
-        isEnemy: true,
-        isDefeated: false,
-        attack: function () {
-            if (isEnemy) {
-                hero.hp  = hero.hp - this.counterAtk;
+            this.displayCombatants = function () {
+                $('#left-img').attr('src', this.left.imgURL);
+                $('#left-name').text(this.left.name)
+                $('#right-img').attr('src', this.right.imgURL)
+                $('#right-name').text(this.right.name)
             }
+    }
 
-            else {
-                enemy.hp = enemy.hp - this.atk; 
-            };
+    var battle = new Battle(hero, enemy);
+    battle.start();
 
-        },
-        checkifDefeated: function() {
-            if (hp == 0) {
-                this.isDefeated = true;
-            }     
-
-        },
-    };
 
     // click event for hero selection. clicking the button should assign the char obj to a var of either hero or enemy
-    $("#rey-btn").on("click", function() {
-        if (hero = undefined) {
+    $("#rey-btn").on("click", function () {
+        if (!hero) {
             hero = rey;
-            console.log(hi)
+            console.log(hero);
         }
         else {
             enemy = rey;
-            rey.isEnemy = true;
+           
         };
     })
 
-    $("#kylo-btn").click(function() {
-        if (hero = undefined) {
+    $("#kylo-btn").on("click", function () {
+        if (!hero) {
             hero = kylo;
+            console.log(hero);
         }
         else {
             enemy = kylo;
-            kylo.isEnemy = true;
+           
         };
     })
 
-    $("#maul-btn").click(function() {
-        if (hero = undefined) {
+    $("#maul-btn").on("click", function () {
+        if (!hero) {
             hero = maul;
+            console.log(hero);
         }
         else {
-            enemy = maul
-            maul.isEnemy = true;
+            enemy = maul;
+        
+            console.log(enemy);
         };
     })
 
-    $("#vader-btn").click(function() {
-        if (hero = undefined) {
+    $("#vader-btn").on("click", function () {
+        if (!hero) {
             hero = vader;
+            console.log(hero);
         }
         else {
             enemy = vader;
-            vader.isEnemy = true;
         };
     })
+
+
+    $("#attack-btn").on("click", function() {
+
+    });
+
 
 })    
