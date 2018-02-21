80baa3c0dc48403af92e668e07676d57ddb57214
oxygenplug <dan.garces2@gmail.com>
2018-02-20 20:51:32 -0500

    can select characters nows wooh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
-    var hero;
-    var enemy;
 
     function Character(hp, atk, counterAtk, name, imgURL) {
         this.hp = hp;
         this.atk = atk;
         this.counterAtk = counterAtk;
         this.isDefeated = false;
-        this.name = "";
-        this.imgURL = "";
-        this.attack = function () {
-            if (isEnemy) {
-                hero.hp = hero.hp - this.counterAtk;
+        this.name = name;
+        this.imgURL = "./assets/images/" + imgURL;
+        this.attack = function (enemy, isCounterAtk) {
+
+            if (isCounterAtk) {
+                this.enemy -= this.counterAtk;
             }
 
             else {
-                enemy.hp = enemy.hp - this.atk;
-            };
+                this.enemy -= this.attack;
+            }
+
+
 
         };
         this.checkifDefeated = function () {
@@ -25,88 +26,108 @@ $(document).ready(function () {
             }
         };
     }
-// for some reason I can't get the name and imgURL to pass through. they come up as empty strings.
-    var rey = new Character(120, 10, 7, "Rey", "../images/rey.jpg");
-    var kylo = new Character(100, 11, 9, "Kylo Ren", "../images/kyloRen.jpg");
-    var maul = new Character(60, 20, 15, "Darth Maul", "../images/darthMaul.png");
-    var vader = new Character(80, 18, 10, "Darth Vader", "../images/vader.jpg");
+    // for some reason I can't get the name and imgURL to pass through. they come up as empty strings.
+    var rey = new Character(120, 10, 7, "Rey", "rey.jpg");
+    var kylo = new Character(100, 11, 9, "Kylo Ren", "kyloRen.jpg");
+    var maul = new Character(60, 20, 15, "Darth Maul", "darthMaul.png");
+    var vader = new Character(80, 18, 10, "Darth Vader", "vader.jpg");
 
 
     // this part would handle the entire "battle" set up sequence, such as dynamically displaying the player and current enemy's images
 
-  /*  function Battle(left, right) {
-        this.left = hero;
-        this.right = enemy;
-        this.start = function (left, right) {
-            this.left = left;
-            this.right = right;
+    function Battle() {
+        this.hero = null;
+        this.enemy = null;
+        this.setHero = function (character) {
+            this.hero = character;
+        };
+        this.setEnemy = function (character) {
+            this.enemy = character;
+        };
+        this.start = function () {
             this.displayCombatants();
+            $("#attack-btn").on("click", this, this.heroAttack);
 
-        },
-            this.displayCombatants = function () {
-                $('#left-img').attr('src', this.left.imgURL);
-                $('#left-name').text(this.left.name)
-                $('#right-img').attr('src', this.right.imgURL)
-                $('#right-name').text(this.right.name)
-            }
-    };  */
+        };
+        this.displayCombatants = function () {
+            console.log("here");
+            $('#hero-img').attr('src', this.hero.imgURL);
+            $('#enemy-img').attr('src', this.enemy.imgURL);
+            this.displayHp();
+            this.toggleBattle();
+        };
+        this.toggleBattle = function () {
+            $('#battleground').toggleClass("hidden");
+        };
 
-  /* CURRENTLY NOT WORKING
-    var battle = new Battle(left, right);
-    battle.start();
-*/
+        this.heroAttack = function (event) {
+            var thisBattle = event.data;
+            thisBattle.hero.attack(this.enemy, false);
+            thisBattle.displayHp();
+        };
+
+        this.displayHp = function () {
+            $('#hero-hp').text(this.hero.hp);
+            $('#enemy-hp').text(this.enemy.hp);
+            
+        };
+
+    };
+
+    // CURRENTLY NOT WORKING
+    var battle = new Battle();
+
 
     // click event for hero selection. clicking the button should assign the char  to a var of either hero or enemy
     $("#rey-btn").on("click", function () {
-        if (!hero) {
-            hero = rey;
-            console.log(hero);
+        if (!battle.hero) {
+            battle.setHero(rey);
+            console.log(battle.hero);
         }
         else {
-            enemy = rey;
-           
+            battle.setEnemy(rey);
+            battle.start();
+
         };
     })
 
     $("#kylo-btn").on("click", function () {
-        if (!hero) {
-            hero = kylo;
-            console.log(hero);
+        if (!battle.hero) {
+            battle.setHero(kylo);
         }
         else {
-            enemy = kylo;
-           
+            battle.setEnemy(kylo);
+            battle.start();
+
         };
     })
 
     $("#maul-btn").on("click", function () {
-        if (!hero) {
-            hero = maul;
-            console.log(hero);
+        if (!battle.hero) {
+            battle.setHero(maul);
         }
         else {
-            enemy = maul;
-        
-            console.log(enemy);
+            battle.setEnemy(maul);
+            battle.start();
+
         };
     })
 
     $("#vader-btn").on("click", function () {
-        if (!hero) {
-            hero = vader;
-            console.log(hero);
+        if (!battle.hero) {
+            battle.setHero(vader);
+
         }
         else {
-            enemy = vader;
+            battle.setEnemy(vader);
+            battle.start();
         };
     })
 
-/* calls the attack function from the Character constructor. this function first checks to see
- if a character is the player or an enemy then peforms either a attack or counter attack
- and subtracts that from the other character's hp pool */
-    $("#attack-btn").on("click", function() {
-        Character.attack();
-    });
+    /* calls the attack function from the Character constructor. this function first checks to see
+     if a character is the player or an enemy then peforms either a attack or counter attack
+     and subtracts that from the other character's hp pool */
+
 
 
 })    
